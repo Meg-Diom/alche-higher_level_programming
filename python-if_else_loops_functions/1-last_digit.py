086a6ade6c4d8a83fbe8fb5e59c1c66e11486e0b
Meg-Diom <d.ngum1@alustudent.com>
2026-08-11 10:59:41 -0700

    Printing the last digit of a bumber

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
-    num =abs(number) % 10
-    num = -num
-if num > 5:
-    print(f"Last digit of {number} is {num} and is greater than 5")
-elif num == 0:
-    print(f"Last digit of {number} is {num} and is 0")
+last_digit = number % 10 if number >= 0 else -(-number % 10)
+if last_digit > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+elif last_digit == 0:
+    print("Last digit of {} is {} and is 0".format(number, last_digit))
 else:
-    print(f"Last digit of {number} is {num} and is less than 6 and not 0")
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
