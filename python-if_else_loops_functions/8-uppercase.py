447e7620cd59399ac359535f0d472eb826f2831a
Meg-Diom <d.ngum1@alustudent.com>
2026-08-13 04:45:41 -0700

    Prnting uppercase strings

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result = ""
-
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            result += chr(ord(c) - 32)
+            print(chr(ord(c) - 32), end="")
         else:
-            result += c
-
-    print(f"{result}")
+            print(c, end="")
+    print()
