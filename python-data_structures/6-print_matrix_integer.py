5efa0884255570d6deb50ed3992f37e3735cc5b8
Meg-Diom <d.ngum1@alustudent.com>
2026-08-17 14:59:24 +0400

    Displaying a matrix in python

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
+
+
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for number in range(len(row)):
-            if number !=0:
-                print(" ", end="")
-            print("{:d}".format(row[number]), end=" ")
-        print()
+        print(" ".join("{:d}".format(number) for number in row))
