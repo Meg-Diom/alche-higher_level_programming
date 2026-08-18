1477524ebdef7b68e42f98fc6a82952adba717a6
Meg-Diom <d.ngum1@alustudent.com>
2026-08-18 22:26:29 +0400

    Displaying the square of each value in a matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -7,7 +7,7 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         new_row = []
         for value in row:
-            new_row.append(row(value ** 2))
+            new_row.append(value ** 2)
         new_matrix.append(new_row)
 
     return new_matrix
