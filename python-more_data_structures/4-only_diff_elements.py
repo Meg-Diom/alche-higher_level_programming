9bf561f8ccc8f593948780dcb6e9625072df4531
Meg-Diom <d.ngum1@alustudent.com>
2026-08-19 16:08:13 +0400

    Displaying a set of unique elements between 2 sets

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -8,5 +8,9 @@ def only_diff_elements(set_1, set_2):
         if element not in set_2:
 
             new_set.add(element)
+    for element in set_2:
+        if element not in set_1:
 
+            new_set.add(element)
+            
     return new_set
