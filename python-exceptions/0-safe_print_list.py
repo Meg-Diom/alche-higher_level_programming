8fe0ec6b6a25b55045982987fc96268f2d83d605
Meg-Diom <d.ngum1@alustudent.com>
2026-08-26 01:28:04 +0400

    Catching errors

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
 
-    i = 0
+    count = 0
     for j in range(x):
         try:
             print(my_list[j], end="")
-            i += 1
+            count += 1
         except IndexError:
             break
 
     print()
-    retturn i
+    retturn count
