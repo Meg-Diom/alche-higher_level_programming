1318810b24be5cd1832d7572a96521e741eb4781
Meg-Diom <d.ngum1@alustudent.com>
2026-08-27 23:03:31 +0400

    Defining a class square that takes argument

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents the square"""
+    def __init__(self, size):
+        """Initializes a new square with arguements"""
+        self.__size = size
