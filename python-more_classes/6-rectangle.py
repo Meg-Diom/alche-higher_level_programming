d312687134eddfa26182265bd5c4fddf656e5acd
Meg-Diom <d.ngum1@alustudent.com>
2026-08-29 20:41:24 +0400

    Changing instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,9 +7,9 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        number_of_instances += 1
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -59,9 +59,9 @@ class Rectangle:
 
     def __repr__(self):
         """Representing the rectangle"""
-        number_of_instances -= 1
         return f"Rectangle({self.__width}, {self.__height})"
 
 def __del__(self):
     """ Exiting """
+    Rectangle.number_of_instances -= 1
     print("Bye rectangle...")
