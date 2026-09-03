10ddf25cf27a5bb640da136d7fb75ba4c19e4216
Meg-Diom <d.ngum1@alustudent.com>
2026-09-03 16:46:31 +0400

    Creating a class with a method

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class ,MyList that inherits from list"""
+
+
+class MyList(list):
+"""Defining the class with a method"""
+
+
+    def print_sorted(self):
+        print(sorted(self))
