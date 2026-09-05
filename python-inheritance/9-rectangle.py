1b9d3245ff37f0eca82454e7ae9e974053f03c6f
Meg-Diom <d.ngum1@alustudent.com>
2026-09-05 22:29:04 +0400

    returning the area of a rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module defining class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defining inheriting class"""
+
+    def __init__(self, width, height):
+        """initializing a rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """returning the rectsngle"""
+        return "[Rectangle] {}/{}".format(self.__width. self.__height)
