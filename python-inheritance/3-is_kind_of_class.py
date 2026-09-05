956afdcf9be15e6d02e890ded4ed5e245eace3ab
Meg-Diom <d.ngum1@alustudent.com>
2026-09-05 20:41:21 +0400

    checking if an object is an instance

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,6 +2,6 @@
 """Checking if an object is an instance"""
 
 
-def is_kind_of_class(obj, s_class):
+def is_kind_of_class(obj, a_class):
     """Defining the function that checks the object"""
     return isinstance(obj, a_class)
