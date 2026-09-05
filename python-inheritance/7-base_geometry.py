6e6b857dd8debb1c80178e2304c2464d45c56f7a
Meg-Diom <d.ngum1@alustudent.com>
2026-09-05 21:31:57 +0400

    Integer validator

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines a class"""
+
+
+class BaseGeometry:
+
+    def area(self):
+        """Raising error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Error raising"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
