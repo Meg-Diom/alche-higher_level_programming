799a9f60765dbc60453e39047e6abb7c4916f350
Meg-Diom <d.ngum1@alustudent.com>
2026-09-05 20:37:53 +0400

    checking if an object is an instance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Defines a function that checks if an object is exactly an instance of a class."""
+"""Defines a function that checks if an object is exactly an
+instance of a class."""
 
 
 def is_same_class(obj, a_class):
---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Checking if an object is an instance"""
+
+
+def is_kind_of_class(obj, s_class):
+    """Defining the function that checks the object"""
+    return isinstance(obj, a_class)
