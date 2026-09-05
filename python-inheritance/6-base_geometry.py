2537e025669c7e305e9c6b9fe9105b1c40c73d35
Meg-Diom <d.ngum1@alustudent.com>
2026-09-05 21:08:50 +0400

    Public instance method that raises errors

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module definition"""
+
+
+class BaseGeometry:
+    """Class definition"""
+
+    def area(self):
+        """Method raising an error"""
+        raise ValueError("area() is not implemented")
