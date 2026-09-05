641b9173002bd299245a2728d8eb439bf84fbf0a
Meg-Diom <d.ngum1@alustudent.com>
2026-09-05 22:49:44 +0400

    Araa of square

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,11 +5,12 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Class definition"""
+
     def __init__(self, size):
         """Initialization"""
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(self, size)
+        super().__init__(size, size)
 
     def area(self):
         """public instance method"""
