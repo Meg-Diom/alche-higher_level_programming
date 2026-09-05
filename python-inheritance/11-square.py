3356b5649b27bec5d03fb22576f130e7d4c863e8
Meg-Diom <d.ngum1@alustudent.com>
2026-09-05 23:01:25 +0400

    Returning the area of a rectangle

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module Inintialization"""
+Rectangle = __init__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Definition of class Square"""
+
+    def __init__(self, size):
+        """Initializatin of square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """public instance method"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """returning a square"""
+        return "[Square] {}/{}".format(self.__size' self.__size)
