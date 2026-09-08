9a648631e5441f3170502a5adaf21c83183d7709
Meg-Diom <d.ngum1@alustudent.com>
2026-09-09 02:05:42 +0400

    Reading a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Reading files in python"""
 
 
 def read_file(filename=""):
+    """Defining a function that takes a file nane as input and reads the file"""
     with open("filename", "r", encoding="utf-8") as file:
         print(file.read(), end=")
