b3a8ba143000e39ac51bffd4fd98d9b888bc5d2b
Meg-Diom <d.ngum1@alustudent.com>
2026-09-09 02:57:28 +0400

    converting from python to JSON

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Converting from python to json strins"""
+"""Converting from python to json string"""
 
 
 import json
 
 def to_json_string(my_obj):
     """Defining the function that converts to json string"""
-    return json.dumps(my_obj"
+    return json.dumps(my_obj)
