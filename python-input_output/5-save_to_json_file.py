4ca87aaa0c4abe6e313b904f77734b0c874e2b90
Meg-Diom <d.ngum1@alustudent.com>
2026-09-09 23:37:58 +0400

    Writing into a json file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Defining the function"""
     with open(filename, "w", encoding="utf-8") as file:
-        json.dump(my_obj, filename)
+        json.dump(my_obj, file)
